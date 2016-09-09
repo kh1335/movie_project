d13ecc387059892ded2b23caa17041b8b1c6926d
kh1335 <kristen.hiserote@gmail.com>
2016-09-08 20:08:34 -0500

    add create and list pages

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,161 @@
 import React from 'react';
 
 class Create extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            movies: [],
+            errors: {title: false, year: false, actors: false},
+            genre: [
+                "Action",
+                "Adventure",
+                "Animation",
+                "Biography",
+                "Comedy",
+                "Crime",
+                "Documentary",
+                "Drama",
+                "Family",
+                "Fantasy",
+                "Film-Noir",
+                "History",
+                "Horror",
+                "Music",
+                "Musical",
+                "Mystery",
+                "Romance",
+                "Sci-Fi",
+                "Sport",
+                "Thriller",
+                "War",
+                "Western"
+            ]
+        }
+    }
+
     render() {
         return (
-            <div> create page </div>
+            <div>
+                <div className="page-header">
+                    <h1>Add New Move</h1>
+                </div>
+                <form className="form-horizontal" onSubmit={this.handleSubmit.bind(this)}>
+                    <div className="text-center text-danger">
+                        {this.state.errors.title || this.state.errors.year || this.state.errors.actors ?
+                            'No fields can be empty.' : ''
+                        }
+                    </div>
+                    <div className={this.state.errors.title ? 'form-group has-error' : 'form-group'}>
+                        <label htmlFor="title" className="col-sm-2 control-label">Title</label>
+                        <div className="col-sm-8">
+                            <input type="text" ref="title" className="form-control" id="title" />
+                        </div>
+                    </div>
+                    <div className={this.state.errors.year ? 'form-group has-error' : 'form-group'}>
+                        <label htmlFor="year" className="col-sm-2 control-label">Year</label>
+                        <div className="col-sm-2">
+                            <input type="number" ref="year" className="form-control" id="year" />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="genre" className="col-sm-2 control-label">Genre</label>
+                        <div className="col-sm-3">
+                            <select id="genre" ref="genre" className="form-control">
+                                {this.state.genre.map((genre, index) => {
+                                    return(
+                                        <option key={index} value={genre}>{genre}</option>
+                                    )
+                                })}
+                            </select>
+                        </div>
+                    </div>
+                    <div className={this.state.errors.actors ? 'form-group has-error' : 'form-group'}>
+                        <label htmlFor="actors" className="col-sm-2 control-label">Actors</label>
+                        <div className="col-sm-8">
+                            <textarea id="actors" ref="actors" className="form-control" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="rating" className="col-sm-2 control-label">Rating</label>
+                        <div className="col-sm-2">
+                            <select id="rating" ref="rating" className="form-control">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="col-sm-offset-2 col-sm-10">
+                            <button type="submit" className="btn btn-primary">Save</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         )
+    }
+
+    handleSubmit(event) {
+        // submit the form
+        event.preventDefault();
+
+        let title = this.refs.title.value;
+        let year = this.refs.year.value;
+        let genre = this.refs.genre.value;
+        let actors = this.refs.actors.value;
+        let rating = this.refs.rating.value;
+
+        //validate form
+        if(!title.trim() || !year.trim() || !actors.trim()) {
+            this.setState({
+                errors: {
+                    title: !title.trim(),
+                    year: !year.trim(),
+                    actors: !actors.trim()
+                }
+            });
+            return false;
+        }
+
+        // add movie to List
+        let movies = this.state.movies.concat([{
+            title: title,
+            year: year,
+            genre: genre,
+            actors: actors.split(', '),
+            rating: rating
+        }]);
+
+        // set in state
+        this.setState({
+            errors: {title: false, year: false, actors: false},
+            movies: movies
+        });
+
+        // reset the values
+        this.refs.title.value = '';
+        this.refs.year.value = '';
+        this.refs.genre.selectedIndex = 0;
+        this.refs.actors.value = '';
+        this.refs.rating.selectedIndex = 0;
+    }
+
+    componentWillMount() {
+        // load movies array from localStorage, set in state
+        let movieList = localStorage.getItem('movies');
+        if (movieList) {
+            this.setState({
+                movies: JSON.parse(movieList)
+            });
+        }
+    }
+
+    componentDidUpdate() {
+        // on each update, sync our state with localStorage
+        localStorage.setItem('movies', JSON.stringify(this.state.movies));
     }
 }
 
--- src/List.js
+++ src/List.js
@@ -1,10 +1,173 @@
 import React from 'react';
 
-class List extends React.Component {
+class Search extends React.Component {
     render() {
         return (
-            <div> List page </div>
+            <div>
+                <form name="search-form" className="form-inline text-center search-form">
+                    <div className="form-group">
+                        <label htmlFor="search-field" className="sr-only control-label">Rating</label>
+                        <div className="col-sm-2">
+                            <select id="search-field" ref="field" className="form-control">
+                                <option value="title">Title</option>
+                                <option value="year">Year</option>
+                                <option value="genre">Genre</option>
+                                <option value="actors">Actors</option>
+                                <option value="rating">Rating</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label className="sr-only" htmlFor="search-term">Search</label>
+                        <div className="input-group">
+                            <input type="text" ref="term" className="form-control" id="search-term" placeholder="Search" onKeyPress={this.handleEnter.bind(this)} />
+                            <div className="btn input-group-addon" onClick={this.handleSearch.bind(this)}><span className="glyphicon glyphicon-search"></span></div>
+                        </div>
+                    </div>
+                    <div className="form-group clear-btn">
+                        <button className="btn btn-primary" ref="clear" onClick={this.handleClearSearch.bind(this)}>Clear Search</button>
+                    </div>
+                </form>
+            </div>
         )
+    }
+
+    handleEnter(event) {
+        // search when enter key is pressed
+        if(event.charCode == 13) {
+            event.preventDefault();
+            this.handleSearch();
+        }
+    }
+
+    handleSearch() {
+        let field = this.refs.field.value;
+        let term = this.refs.term.value;
+
+        // make sure term is not empty
+        if(!term.trim()) {
+            return false;
+        }
+
+        // filter out only movies with field and term
+        let filtered = this.props.movies.filter((movie) => {
+            if(Array.isArray(movie[field])) {
+                return movie[field].filter((item) => {
+                    return item.indexOf(term) > -1
+                }).length > 0;
+            }
+
+            return movie[field].indexOf(term) > -1;
+        });
+
+        this.props.updateList(filtered);
+    }
+
+    handleClearSearch(event) {
+        event.preventDefault();
+
+        // set search fields to default value
+        this.refs.field.selectedIndex = 0;
+        this.refs.term.value = '';
+
+        this.props.updateList([], true);
+    }
+}
+
+class MovieList extends React.Component {
+    render() {
+        return (
+            <div className="row well list">
+                {(this.props.movies.length > 0) ?
+                    this.props.movies.map((movie, index) => {
+                        return (
+                            <div key={index} className="col-md-4">
+                                <div className="panel panel-default">
+                                    <div className="panel-heading clearfix">
+                                        <span data-movieid={index} className="glyphicon glyphicon-trash text-danger pull-right" onClick={this.handleDelete.bind(this)}></span>
+                                        <h3 className="panel-title">{movie.title} ({movie.year})</h3>
+                                    </div>
+                                    <div className="panel-body">
+                                        <div>Genre: {movie.genre}</div>
+                                        <div>Actors: {movie.actors.join(', ')}</div>
+                                        <div>Rating: {movie.rating}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }) :
+                    <div className="text-danger text-center">There Are No Movies To Show</div>
+            }
+            </div>
+        )
+    }
+
+    handleDelete(event) {
+        let movieid = event.target.getAttribute('data-movieid');
+
+        // filter out movie that was deleted
+        let filtered = this.props.movies.filter((movie, index) => {
+            return index != movieid
+        });
+
+        this.props.deleteMovie(filtered);
+    }
+}
+
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            movies: [],
+            searchMovies: []
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="page-header">
+                    <h1>Movie List</h1>
+                </div>
+                <Search movies={this.state.movies}  updateList={this.updateList.bind(this)} />
+                <MovieList movies={this.state.searchMovies} deleteMovie={this.deleteMovie.bind(this)} />
+            </div>
+        )
+    }
+
+    updateList(movies, replace = false) {
+        // update movies to show all or filtered by search
+        if (!replace) {
+            this.setState({
+                searchMovies: movies
+            });
+        } else {
+            this.setState({
+                searchMovies: JSON.parse(localStorage.getItem('movies'))
+            });
+        }
+    }
+
+    deleteMovie(movies) {
+        // set movies and searchMovies and save to localStorage
+        this.setState({
+            movies: movies,
+            searchMovies: movies
+        });
+
+        localStorage.setItem('movies', JSON.stringify(movies));
+    }
+
+    componentWillMount() {
+        // load items array from localStorage, set in state
+        let movieList = localStorage.getItem('movies');
+        if (movieList) {
+            this.setState({
+                movies: JSON.parse(movieList),
+                searchMovies: JSON.parse(movieList)
+            });
+        }
     }
 }
 
