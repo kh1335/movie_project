c6f070c7f1dc4e2d2cd5ab32840089ae99393aee
kh1335 <kristen.hiserote@gmail.com>
2016-09-08 19:10:12 -0500

    create home page

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,14 @@ import React from 'react';
 class Home extends React.Component {
     render() {
         return (
-            <div> home page </div>
+            <div>
+                <div className="jumbotron text-center">
+                    <h1>Movie Project</h1>
+                    <p>Create, Search, and View list of movies in your own movie database</p>
+                    <a className="btn btn-primary home-btn" href="/create" role="button">Add Movie</a>
+                    <a className="btn btn-primary home-btn" href="/list" role="button">View List</a>
+                </div>
+            </div>
         )
     }
 }
